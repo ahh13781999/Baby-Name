3c554158eda580a3d7584184023cdcafc97e12f0
ahh13781999 <ahh13781999@gmail.com>
2023-07-09 18:29:16 +0330

    first commit

---
+++ app.vue
@@ -0,0 +1,130 @@
+<script setup lang="ts">
+import { Gender, Popularity, Length, names } from '@/data'
+
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+
+const obj: OptionsState = {
+  gender: Gender.GIRL,
+  length: Length.ALL,
+  popularity: Popularity.TRENDY,
+}
+
+const options = reactive<OptionsState>({
+  gender: Gender.GIRL,
+  length: Length.ALL,
+  popularity: Popularity.TRENDY,
+})
+
+const selectedNames = ref<string[]>([])
+</script>
+
+<template>
+  <div class="container">
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" button below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button class="option option-left" :class="options.gender === Gender.BOY && 'option-active'"
+            @click="options.gender = Gender.BOY">Boy</button>
+          <button class="option" :class="options.gender === Gender.UNISEX && 'option-active'"
+            @click="options.gender = Gender.UNISEX">Unisex</button>
+          <button class="option option-right" :class="options.gender === Gender.GIRL && 'option-active'"
+            @click="options.gender = Gender.GIRL">Girl</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <button class="option option-left" :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="options.popularity = Popularity.TRENDY">Trendy</button>
+          <button class="option option-right" :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="options.popularity = Popularity.UNIQUE">Unique</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3) Choose name's length</h4>
+        <div class="option-buttons">
+          <button class="option option-left" :class="options.length === Length.LONG && 'option-active'"
+            @click="options.length = Length.LONG">Long</button>
+          <button class="option" :class="options.length === Length.ALL && 'option-active'"
+            @click="options.length = Length.ALL">All</button>
+          <button class="option option-right" :class="options.length === Length.SHORT && 'option-active'"
+            @click="options.length = Length.SHORT">Short</button>
+        </div>
+      </div>
+      <button class="primary">Find Names</button>
+    </div>
+    {{ selectedNames }}
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+.options-container {
+  background-color: rgb(255, 238, 326);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.option {
+  background-color: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+}
+
+.option-left {
+  border-radius: 1rem 0 0 1rem;
+
+}
+
+.option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white
+}
+
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border-radius: 6.5rem;
+  border: none;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+</style>
